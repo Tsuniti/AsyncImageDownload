5a8c17a25dc21e30ad7186b30cc15043fc0d9fbc
Tsuniti <Tsuniti00@gmail.com>
2024-08-24 10:00:19 +0300

    Add project files.

---
+++ ImageDownloader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncImageDownload;
+
+public  class ImageDownloader
+{
+    HttpClient client = new HttpClient();
+
+    public async Task<byte[]> DownloadImageAsync(string url) => await client.GetByteArrayAsync(url);
+
+
+    public async Task SaveImageAsync(string savePath, byte[] imageData) => await File.WriteAllBytesAsync(savePath, imageData);
+    
+    public async Task DownloadAndSaveImagesAsyn(string savePath, IEnumerable<string> urls)
+    {
+        foreach (string url in urls)
+        {
+            await SaveImageAsync(savePath + url.Split('/').Last().Split(".")[0] + ".jpg", await DownloadImageAsync(url));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AsyncImageDownload;
+using System;
+
+string imageUrl = "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-980x653.jpg";
+List<string> imagesUrls = new List<string>
+{
+    "https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg",
+    "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
+    "https://t4.ftcdn.net/jpg/02/56/10/07/360_F_256100731_qNLp6MQ3FjYtA3Freu9epjhsAj2cwU9c.jpg",
+    "https://fps.cdnpk.net/images/home/subhome-ai.webp?w=649&h=649"
+};
+string savePath = "C:\\Users\\Tsuni\\Desktop\\Test\\";
+
+var Downloader = new ImageDownloader();
+//try
+//{
+//    var imageData = await Downloader.DownloadImageAsync(imageUrl);
+//    Console.WriteLine("Изображение успешно скачано");
+//    await Downloader.SaveImageAsync(savePath + imageUrl.Split('/').Last().Split(".")[0] + ".jpg", imageData);
+//    Console.WriteLine("Изображение успешно сохранено.");
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine($"Ошибка: {ex.Message}");
+//}
+
+try
+{
+    await Downloader.DownloadAndSaveImagesAsyn(savePath, imagesUrls);
+    Console.WriteLine("All images saved");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка: {ex.Message}");
+}
